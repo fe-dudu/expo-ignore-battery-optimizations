c0d07d0e2b0b7ff07801da0c1ed24331f98e6f3c
fe-dudu <ehehwhdwhd@naver.com>
2026-05-15 17:21:45 +0000

    chore: version packages

--- plugin/build/withBatteryOptimizationPermissions.js
+++ plugin/build/withBatteryOptimizationPermissions.js
@@ -4,8 +4,6 @@ const config_plugins_1 = require("expo/config-plugins");
 const pkg = require('../../package.json');
 const BATTERY_OPT_PERMISSION = 'android.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS';
 const withBatteryOptimizationPermission = (config) => {
-    return config_plugins_1.AndroidConfig.Permissions.withPermissions(config, [
-        BATTERY_OPT_PERMISSION,
-    ]);
+    return config_plugins_1.AndroidConfig.Permissions.withPermissions(config, [BATTERY_OPT_PERMISSION]);
 };
 exports.default = (0, config_plugins_1.createRunOncePlugin)(withBatteryOptimizationPermission, pkg.name, pkg.version);
