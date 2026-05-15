3eceeb31151b9e33e1d1185a0a42219b3eb47301
김도현 <ehehwhdwhd@naver.com>
2026-05-16 02:03:11 +0900

    chore(build): stop tracking generated output

--- build/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @platform android
- */
-export declare function isIgnoringBatteryOptimizations(): boolean;
-/**
- * @platform android
- */
-export declare function requestIgnoreBatteryOptimizations(): Promise<void>;
-//# sourceMappingURL=index.d.ts.map
